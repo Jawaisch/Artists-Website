814c95bdea2e3b0c40ec78a83e50825a05c302d4
Coreschaden <45372361+Coreschaden@users.noreply.github.com>
2019-05-29 23:22:57 +0200

    Update public_artist.php

--- Modularisation/public_artist.php
+++ Modularisation/public_artist.php
@@ -29,21 +29,6 @@
 					  "		  kuenstler.Vita ".
 					  "FROM	  kuenstler ".
 			          "WHERE  kuenstler.Kuenstler_ID = ?";
-					  
-
-					  /*"	  bild.Bild_ID, ".
-					  "		  bild.Mal_Technik, ".
-					  "		  bild.Titel, ".
-					  "       bild.Hoehe, ".
-					  "       bild.Breite, ".
-					  "       bild.VK_Preis, ".
-					  "       bild.Einstell_Zeitstempel, ".
-					  "		  genre.Genre_ID, ".
-					  "		  genre.Name ".
-					  INNER JOIN bild USING (Kuenstler_ID) ".
-					  "				  INNER JOIN eingeordnet USING (Bild_ID) ".
-					  "				  INNER JOIN genre USING (Genre_ID) ".
-					  " ORDER BY bild.Einstell_Zeitstempel DESC";*/
 
 	// Abfrage an DB-Server senden
 		$result_handle = $prepstmt->prepare( $SQL_String );
@@ -80,21 +65,97 @@
 		return $Public_Artist_Arr;
 	}
 
-	function DisplayPublicData($PublicData)
+	function GetAllArtistPics( $dbconn, $kid )
 	{
-	echo "<div class=\"PublicArtistInfo\">"."\n".
-			 "<div class=\"PublicProfilPic\">".
-			 "<img src=\"art-images/small/752.png\" alt=\"Profilbild\"/>".
-			 "</div>".
-			 "<div class=\"PublicArtistVita\">".
-			 "  <h3>K&uuml;nstler-Profil von ".$PublicData[0]['Kname']."</h3>"."\n".
-			 "<p>".$PublicData[0]['Vita']."</p> ".
-			 "</div>".
-		
-		 "</div>"."\n";
+	  $prepstmt = $dbconn->stmt_init();
 
+		$SQL_String = " SELECT bild.Bild_ID,".
+					  "		   bild.Titel".
+					  " FROM bild INNER JOIN Kuenstler USING (Kuenstler_ID)".
+					  " WHERE bild.Kuenstler_ID = ? ".
+					  " ORDER BY bild.Einstell_Zeitstempel DESC";
+
+		// Abfrage an DB-Server senden
+		$result_handle = $prepstmt->prepare( $SQL_String );
+
+		// Abfrage auf Erfolg Ueberpruefen
+		if($result_handle === false)
+		{
+			ErrorHandling($SQL_String, $dbconn);
+		}
+		else
+		{	
+			//4. Daten an die Parameter binden
+			$prepstmt->bind_param( "i" ,$kid);
+
+			 //5. Diese Parameter anwenden
+			$prepstmt->execute();
+
+			$Public_Artist_Arr = array();  // ein leeres Array initialisieren!
+
+			$result_handle = $prepstmt->get_result( );
+
+			// fetch-en Spezialfall: ein oder kein Datensatz
+
+			$Public_Artist_Arr = $result_handle->fetch_all( MYSQLI_ASSOC );
+
+			if($Public_Artist_Arr == NULL)
+			{
+				$Public_Artist_Arr = false;
+			}					
+		}
+
+		$prepstmt->close(); //abräumen nach getaner Arbeit
+		
+		return $Public_Artist_Arr;
 	}
-	
+
+
+	function DisplayPublicData($PublicData,$PublicPics)
+	{
+		$SID = session_id();
+		$Kname = $PublicData[0]['Kname'];
+		$Kvita = $PublicData[0]['Vita'];
+		$LastPic = $PublicPics[0]['Bild_ID'];
+
+		echo <<<PUBLIC_CONTENT
+				<div class="PublicArtistInfo">
+					<div class="PublicProfilPic">
+						<img src="art-images/small/$LastPic.png" alt="Profilbild" />
+					</div>
+					<div class="PublicArtistVita">
+						<h3>&Ouml;ffentliches K&uuml;nstler-Profil von $Kname</h3>
+						<p>$Kvita</p>
+					</div>
+				</div>
+PUBLIC_CONTENT;
+
+
+		if(!empty($PublicPics))
+		{
+			foreach( $PublicPics AS $PicInfo )
+			{	
+				$bid = $PicInfo['Bild_ID'];
+				$btitle = $PicInfo['Titel'];
+				
+			echo <<<ARTISTPICS
+					<div class="gallery_content">
+						<div class="image">
+							<img src="art-images/small/$bid.png" alt="$btitle"/>
+						</div>
+						<div class="image_notes">
+							<h4>$btitle</h4>
+							<p><a href="gross_bild.php?kws=$SID&amp;bid=$bid">Genauer betrachten</a>
+							</p>
+						</div>
+					</div>
+ARTISTPICS;
+			}
+		}
+
+		
+	}
+	 	
 
   
 ?>
@@ -103,10 +164,8 @@
   <?php
 		ErrorOccurred( );
 		$PublicData = GetPublicArtistData($dbconn, $_GET['kid']);
-		DisplayPublicData( $PublicData);
-		
-		
-
+		$PublicPics = GetAllArtistPics($dbconn, $_GET['kid']);
+		DisplayPublicData( $PublicData, $PublicPics);
   ?>
 		
 
