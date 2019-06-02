c8656ee07cf7f752914089644ce2588b9ee20a41
Coreschaden <45372361+Coreschaden@users.noreply.github.com>
2019-06-02 14:43:06 +0200

    Update All_Artists.php

--- Modularisation/All_Artists.php
+++ Modularisation/All_Artists.php
@@ -3,47 +3,31 @@
 
   include_once( "./includes/view.inc" );
   include_once( "./includes/php.inc" );
-  include_once( "./includes/data.inc" );   
+  include_once( "./includes/data.inc" );
   InitSession();  // Nimmt die aktuelle Session wieder auf
   $_SESSION['referer'] = $_SERVER['PHP_SELF'];
   // CheckLogin();   // Überprüft auf eine erfolgreiche Anmeldung. Nur auf Seiten die nicht von Gästen gesehen werden dürfen!
 
   $dbconn = KWS_DB_Connect( $_SESSION['login']['user'] ); // Datenbankverbindung
-  
+
   PrintHtmlHeader( );
   PrintHtmlTopnav( $_SERVER['PHP_SELF'], SID );
 
   /*#########################################################################
         BEGINN DES CONTENTS
     #######################################################################*/
-  function DisplayAllArtists($Artist_Arr)
-  {
-    echo "<div class=\"table\">". 
-         "\n      <table>".
-         "\n        <tr><th>Unsere K&uuml;nstler</th></tr>\n";
-        
-    foreach ( $Artist_Arr AS $Artists )
-      {  
-      
-       echo "         <tr><td><a href=\"public_artist.php?".SID."&amp;kid=".$Artists['Kuenstler_ID']."\">".$Artists['KName']."</a></td></tr>\n";   
-      }
-    
-    echo  "       </table></div>";
-  }
-
 ?>
 
   <div id="content">
-  
+
     <?php
     $Artists = GetAllArtists($dbconn);
-    DebugArr($Artists);
     DisplayAllArtists($Artists);
   ?>
 
   <div class="clearBoth" >&nbsp;</div>
   </div>
-  <!-- end #content --> 
+  <!-- end #content -->
 
 <?php
   /*#########################################################################
@@ -51,5 +35,5 @@
     #######################################################################*/
 
   PrintHtmlSidebar( $_SESSION['login']['user'], SID );
-  PrintHtmlFooter( SID ); 
+  PrintHtmlFooter( SID );
 ?>
