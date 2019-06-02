b6e490d6e2f154e68911b2e89c65bfe62c0f27ba
Coreschaden <45372361+Coreschaden@users.noreply.github.com>
2019-06-02 22:33:09 +0200

    Create Verkaufsuebersicht.php

---
+++ Modularisation/Verkaufsuebersicht.php
@@ -0,0 +1,43 @@
+<?php
+  error_reporting( E_ALL );
+
+  include_once( "./includes/view.inc" );
+  include_once( "./includes/php.inc" );
+  include_once( "./includes/data.inc" );
+  include_once( "./includes/inputCheck.inc" );
+  InitSession();  // Nimmt die aktuelle Session wieder auf
+  $_SESSION['referer'] = $_SERVER['PHP_SELF'];
+  CheckLogin();   // Überprüft auf eine erfolgreiche Anmeldung. Nur auf Seiten die nicht von Gästen gesehen werden dürfen!
+  if( $_SESSION['login']['user'] === "kws_kunde")
+  {
+    header( 'Location: '.$_SESSION['referer'].'?'.SID);
+  }
+  $dbconn = KWS_DB_Connect( $_SESSION['login']['user'] ); // Datenbankverbindung
+
+  PrintHtmlHeader( );
+  PrintHtmlTopnav( $_SERVER['PHP_SELF'], SID );
+
+  /*#########################################################################
+        BEGINN DES CONTENTS
+    #######################################################################*/
+?>
+
+  <div id="content">
+
+    <?php
+      $SoldPics = GetAllSoldPics($dbconn, $_SESSION['login']['KID']);
+      DisplaySoldPics($SoldPics)
+    ?>
+
+	<div class="clearBoth" >&nbsp;</div>
+  </div>
+  <!-- end #content -->
+
+<?php
+  /*#########################################################################
+          ENDE DES CONTENTS
+    #######################################################################*/
+
+  PrintHtmlSidebar( $_SESSION['login']['user'], SID );
+  PrintHtmlFooter( SID );
+?>
