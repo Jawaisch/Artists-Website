2fedd3ac6fb70ededa58858f2bd52db4fed0aee4
Coreschaden <45372361+Coreschaden@users.noreply.github.com>
2019-06-02 19:39:30 +0200

    Update kaufbestaetigung.php

--- Modularisation/kaufbestaetigung.php
+++ Modularisation/kaufbestaetigung.php
@@ -7,8 +7,6 @@
   InitSession();  // Nimmt die aktuelle Session wieder auf
   $_SESSION['referer'] = $_SERVER['PHP_SELF'];
   CheckLogin();   // Überprüft auf eine erfolgreiche Anmeldung. Nur auf Seiten die nicht von Gästen gesehen werden dürfen!
-
-  $dbconn = KWS_DB_Connect( $_SESSION['login']['user'] ); // Datenbankverbindung
   
   PrintHtmlHeader( );
   PrintHtmlTopnav( $_SERVER['PHP_SELF'], SID );
@@ -16,39 +14,19 @@
   /*#########################################################################
         BEGINN DES CONTENTS
     #######################################################################*/
-?>
-  <div id="content">
-  <?php
-    
-    $con = mysqli_connect("localhost","kws_kunde","kws_kunde","19ss_tedk4_kws");
-    $sql_abfrage = "UPDATE  bild
-                    SET     bild.Kauf_Zeitstempel = CURRENT_TIMESTAMP(),
-                            bild.User_ID = '".$_SESSION['login']['UID']."',
-                            bild.Kauf_IP = '".$_SERVER['REMOTE_ADDR']."'
-                    
-                    WHERE Bild_ID IN ('".$_SESSION['cartimpl']."')";
-    // $_SESSION['login']['UID']
-    $res = mysqli_query($con,$sql_abfrage);
-    if ($res==true)    
-    ?>
 
-    <div class="success">
-      <br />Kauf abgeschlossen<br /><br />
-    </div>
+		// Datenbankverbindung
+		$dbconn = KWS_DB_Connect( "kunde" ); 
 
-    Bitte überweisen Sie den Betrag von <?php echo $_POST['purchase_sum'] ?>€ auf das folgende Konto:<br />
-    <pre>
-    Zahlungsempfänger: Art Tick GmbH, Musterstr. 5 in 10243 Musterhausen    
-    Bank: MusterFinance
-    IBAN: CH82 0900 0000 6035 9126 4    
-    Verwendungszweck: art_nr <?php echo $_SESSION['cart'][0] ?>
-    </pre>
-    <br />
-    <?php $_SESSION['cart'] = NULL;  $_SESSION['cartimpl'] = NULL ; ?>   
+		// gekaufte Bilder mit dem Kunden verknüpfen
+		UpdateImageState( $dbconn );
+		
+		// Zahlungsaufforderung
+		HtmlPrintPaymentRequest( );
 
-	<div class="clearBoth" >&nbsp;</div>
-  </div> 
-<?php
+		// den Warenkorb löschen
+		$_SESSION['cart'] = NULL;  $_SESSION['cartimpl'] = NULL ;
+
   /*#########################################################################
           ENDE DES CONTENTS
     #######################################################################*/
