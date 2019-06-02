7f516c87ac0d83d4f9e2623e9e4311763f81abde
Jawaisch <r.quinque@web.de>
2019-06-02 12:38:15 +0200

    Revert "Update genre.php"
    This reverts commit 6a86bc8146eec4cbfe6b3b56afd3969d35743276.

--- Modularisation/genre.php
+++ Modularisation/genre.php
@@ -16,88 +16,88 @@
   /*#########################################################################
         BEGINN DES CONTENTS
     #######################################################################*/
-  function GetGenreInfo( $dbconn, $gen )
-  {
-    $prepstmt = $dbconn->stmt_init();
+	function GetGenreInfo( $dbconn, $gen )
+	{
+	  $prepstmt = $dbconn->stmt_init();
 
-    $SQL_String = " SELECT  genre.Name,".
-                  "         genre.Beschreibung".
-                  " FROM    genre ".
-                  " WHERE   genre.Genre_ID = ? ";
+		$SQL_String = " SELECT genre.Name,".
+					  "        genre.Beschreibung".
+					  " FROM genre ".
+					  " WHERE genre.Genre_ID = ? ";
 
-    // Abfrage an DB-Server senden
-    $result_handle = $prepstmt->prepare( $SQL_String );
+		// Abfrage an DB-Server senden
+		$result_handle = $prepstmt->prepare( $SQL_String );
 
-    // Abfrage auf Erfolg Ueberpruefen
-    if($result_handle === false)
-    {
-      ErrorHandling($SQL_String, $dbconn);
-    }
-    else
-    { 
-      //4. Daten an die Parameter binden
-      $prepstmt->bind_param( "i" ,$gen);
+		// Abfrage auf Erfolg Ueberpruefen
+		if($result_handle === false)
+		{
+			ErrorHandling($SQL_String, $dbconn);
+		}
+		else
+		{	
+			//4. Daten an die Parameter binden
+			$prepstmt->bind_param( "i" ,$gen);
 
-       //5. Diese Parameter anwenden
-      $prepstmt->execute();
+			 //5. Diese Parameter anwenden
+			$prepstmt->execute();
 
-      $Genre_Note = array();  // ein leeres Array initialisieren!
+			$Genre_Note = array();  // ein leeres Array initialisieren!
 
-      $result_handle = $prepstmt->get_result( );
+			$result_handle = $prepstmt->get_result( );
 
-      // fetch-en Spezialfall: ein oder kein Datensatz
+			// fetch-en Spezialfall: ein oder kein Datensatz
 
-      $Genre_Note = $result_handle->fetch_all( MYSQLI_ASSOC );
+			$Genre_Note = $result_handle->fetch_all( MYSQLI_ASSOC );
 
-      if($Genre_Note == NULL)
-      {
-        $Genre = false;
-      }
-      else
-      {
-        $Genre = $Genre_Note;
-      }
-    }
+			if($Genre_Note == NULL)
+			{
+				$Genre = false;
+			}
+			else
+			{
+				$Genre = $Genre_Note;
+			}
+		}
 
-    $prepstmt->close(); //abräumen nach getaner Arbeit
-    
-    return $Genre;
-  }
+		$prepstmt->close(); //abräumen nach getaner Arbeit
+		
+		return $Genre;
+	}
 
-  function DisplayGenreInfo($GenreInfo)
-  { 
-    $Gname = $GenreInfo[0]['Name']; 
-    
-    // Überprüfen ob bereits eine HTML-Formatierung im String vorliegt
-    if ( preg_match('<p>',$GenreInfo[0]['Beschreibung'])  === false ) 
+	function DisplayGenreInfo($GenreInfo)
+	{	
+		$Gname = $GenreInfo[0]['Name']; 
+		
+		// Überprüfen ob bereits eine HTML-Formatierung im String vorliegt
+		if ( preg_match('<p>',$GenreInfo[0]['Beschreibung'])  === false ) 
         {
-        $Ginfo = "<p>".$GenreInfo[0]['Beschreibung']."</p>";  
+			  $Ginfo = "<p>".$GenreInfo[0]['Beschreibung']."</p>";  
         }
         else
         {      
-        $Ginfo = $GenreInfo[0]['Beschreibung'];
-    }
-    echo <<<GENREBOX
-       <div class="Genre_Info">
-         <h3>$Gname</h3>
-          $Ginfo
-       </div>
+			  $Ginfo = $GenreInfo[0]['Beschreibung'];
+		}
+		echo <<<GENREBOX
+			 <div class="Genre_Info">
+				 <h3>$Gname</h3>
+					$Ginfo
+			 </div>
 GENREBOX;
-  }
-  
+	}
+	
 ?>
 
   <div id="content">
   <?php
-  $GenInfo = GetGenreInfo( $dbconn, $_GET['gen'] );
-  DisplayGenreInfo($GenInfo);
+	$GenInfo = GetGenreInfo( $dbconn, $_GET['gen'] );
+	DisplayGenreInfo($GenInfo);
 
   ?>
 
   
     <!-- Hier kommt euer content rein! -->
 
-  <div class="clearBoth" >&nbsp;</div>
+	<div class="clearBoth" >&nbsp;</div>
   </div>
   <!-- end #content --> 
 
