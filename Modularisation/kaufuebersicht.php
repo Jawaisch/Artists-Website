add1570dbfc28f153b009d8933b54285a02ac255
Coreschaden <45372361+Coreschaden@users.noreply.github.com>
2019-06-02 13:48:04 +0200

    Create kaufuebersicht.php

---
+++ Modularisation/kaufuebersicht.php
@@ -0,0 +1,38 @@
+<?php
+  error_reporting( E_ALL );
+
+  include_once( "./includes/view.inc" );
+  include_once( "./includes/php.inc" );
+  include_once( "./includes/data.inc" );   
+  InitSession();  // Nimmt die aktuelle Session wieder auf
+  $_SESSION['referer'] = $_SERVER['PHP_SELF'];
+  CheckLogin();   // Überprüft auf eine erfolgreiche Anmeldung. Nur auf Seiten die nicht von Gästen gesehen werden dürfen!
+
+  $dbconn = KWS_DB_Connect( $_SESSION['login']['user'] ); // Datenbankverbindung
+  
+  PrintHtmlHeader( );
+  PrintHtmlTopnav( $_SERVER['PHP_SELF'], SID );
+
+  /*#########################################################################
+        BEGINN DES CONTENTS
+    #######################################################################*/
+	$Images = GetUserBuys($dbconn)
+?>
+
+  <div id="content">
+  
+	<h3>Kaufübersicht</h3>
+    <?php PrintHtmlGallery($Images) ?>
+
+	<div class="clearBoth" >&nbsp;</div>
+  </div>
+  <!-- end #content --> 
+
+<?php
+  /*#########################################################################
+          ENDE DES CONTENTS
+    #######################################################################*/
+
+  PrintHtmlSidebar( $_SESSION['login']['user'], SID );
+  PrintHtmlFooter( SID ); 
+?>
